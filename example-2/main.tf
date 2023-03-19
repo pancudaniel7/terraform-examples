ac7ea394d30762f81750754fc4aa4ada7dfef834
Daniel <pancudaniel7@gmail.com>
2023-03-19 23:41:16 +0200

    Added backendfile

--- example-2/main.tf
+++ example-2/main.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+terraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
 }
+
+
+
